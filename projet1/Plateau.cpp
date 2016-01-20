7b70dd697722b4eec65f568487a944fd41889560
root <root@localhost.localdomain>
2016-01-20 11:22:10 +0100

    test

--- projet1/Plateau.cpp
+++ projet1/Plateau.cpp
@@ -1,2 +1,18 @@
 #include	<iostream>
 #include	<string>
+#include"Plateau.h"
+
+Plateau::Plateau(int totalstick, int joueur)
+{
+  p_totalstick = totalstick;
+  p_joueur = joueur;
+    
+  std::cout << "le plateau a été crée !" << std::endl;
+}
+
+Plateau::~Plateau()
+{
+  std::cout << "le plateau est vide, la partie est terminée !" << std::endl;
+}
+
+Plateau&       Plateau::set
